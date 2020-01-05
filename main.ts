24b00218f3c46f84b696e0fb100c27976466694b
zpandly <zpandly@gmail.com>
2020-01-05 21:48:59 +0800

    更新。

--- main.ts
+++ main.ts
@@ -1,7 +1,52 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+enum TrashBinType {
+    //% block="可回收物桶"
+    Recycle = 0,
+    //% block="厨余垃圾桶"
+    Kitchen = 1,
+    //% block="其他垃圾桶"
+    Others = 2,
+    //% block="有害垃圾桶"
+    Harmful = 3
+}
+
+namespace ServoCtrl {
+    
+    //% blockId="ServoCtrl_ServoOpen" block="打开%tbType"
+   export function ServoOpen(tbType: TrashBinType) {
+        switch (tbType)
+        {
+            case TrashBinType.Recycle:
+                servos.P0.setPulse(2000);
+                break;
+            case TrashBinType.Kitchen:
+                servos.P1.setPulse(2000);
+                break;
+            case TrashBinType.Others:
+                servos.P2.setPulse(2000);
+                break;
+            case TrashBinType.Harmful:
+                break;
+            default: 
+                break;
+        }
+    }
+
+    //% blockId="ServoCtrl_ServoClose" block="关闭%tbType"
+   export function ServoClose(tbType: TrashBinType) {
+       switch (tbType) {
+           case TrashBinType.Recycle:
+               servos.P0.setPulse(900);
+               break;
+           case TrashBinType.Kitchen:
+               servos.P1.setPulse(900);
+               break;
+           case TrashBinType.Others:
+               servos.P2.setPulse(900);
+               break;
+           case TrashBinType.Harmful:
+               break;
+           default:
+               break;
+       }
+   }
+}
